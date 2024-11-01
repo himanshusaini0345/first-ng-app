ea0b3b1703877115fbc4b02d5e6a416460fdfa99
Himanshu Saini <himanshusaini0345@outlook.com>
2024-11-01 23:51:40 +0530

    event handlers

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component, signal } from '@angular/core';
+import { GreetingComponent } from '../components/greeting/greeting.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [GreetingComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+  homeMessage = signal('From Home');
+
+  keyUpHandler(event: KeyboardEvent) {
+    console.log(`${event.key} pressed.`);
+  }
+}
